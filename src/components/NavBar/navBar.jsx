d480f3499b00b31e64351e44d21cc2980f7e2972
Tyler <tyler111099@gmail.com>
2023-06-23 16:58:39 -0400

    updates link

--- src/components/NavBar/navBar.jsx
+++ src/components/NavBar/navBar.jsx
@@ -6,11 +6,11 @@ function AppDesign(props) {
 
   return (
     <div>
-        <HashLink smooth to={"/#home"}>Home</HashLink>
-        <HashLink smooth to={"/#about"}>About</HashLink>
-        <HashLink smooth to={"/#portfolio"}>Portfolio</HashLink>
-        <HashLink smooth to={"/#resume"}>Resume</HashLink>
-        <HashLink smooth to={"/#contact"}>Contact</HashLink>
+        <HashLink smooth to={"/KendallBrewer/#home"}>Home</HashLink>
+        <HashLink smooth to={"/KendallBrewer/#about"}>About</HashLink>
+        <HashLink smooth to={"/KendallBrewer/#portfolio"}>Portfolio</HashLink>
+        <HashLink smooth to={"/KendallBrewer/#resume"}>Resume</HashLink>
+        <HashLink smooth to={"/KendallBrewer/#contact"}>Contact</HashLink>
     </div>
   );
 }
