ce890a896edfc05c39f9c160329c3d6ec51eb6a1
Tyler <tyler111099@gmail.com>
2023-06-24 21:04:12 -0400

    adds NavBar and global styles

---
+++ src/components/AppDesign/appDesign.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import './styles.css';
+
+type Props = {
+  
+}
+
+
+function AppDesign(props: Props) {
+  // const {} = props;
+
+  return (
+    <div>
+    </div>
+  );
+}
+
+export default AppDesign;
